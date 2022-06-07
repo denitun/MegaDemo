3e43716860425b3418dd21a83094102838546e2d
denitun <denis.itunin@mail.ru>
2022-06-08 01:10:35 +0300

    ааааааа

--- src/main/java/com/example/demo/Bot.java
+++ src/main/java/com/example/demo/Bot.java
@@ -23,7 +23,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
+//chtob zakomitet smog
 @Component
 @Slf4j
 public class Bot extends TelegramLongPollingBot  {
